d05f71d0434901933695112e4b3e89832755044a
C-Anirudh <canirudh16@gmail.com>
2019-09-22 15:59:12 +0530

    partial implementation

--- pkg/skb/skbuff.go
+++ pkg/skb/skbuff.go
@@ -23,81 +23,128 @@
  * SOFTWARE.
  */
 
- package skb
- 
- import (
-	linkedList "github.com/Aniketh01/probe/pkg/list"
- )
+package skb
 
- // ProbeSkbuff sk_buff routines
- type ProbeSkbuff struct {
-	list linkedList.ProbeList
-	head, end, data, tail, payload *uint8
-	protocol uint16
-	len, dlen, seq, endseq uint32
-	refcount int
- }
+type probeSkbuffHead struct {
+	next, prev *ProbeSkbuff
+	qlen       uint
+}
 
- type probeSkbuffHead struct {
-	 list linkedList.ProbeList
-	 qlen uint32
- }
+// ProbeSkbuff is the implementation of sk_buff
+type ProbeSkbuff struct {
+	next, prev                     *ProbeSkbuff
+	head, end, data, tail, payload uint
+	protocol                       uint
+	len, dlen, seq, endseq         uint
+	refcount                       int
+}
 
-// ProbeSkbuffInit Initialises a sk_buff.
+// ProbeSkbuffInit initialises a sk_buff
 func ProbeSkbuffInit(size uint) *ProbeSkbuff {
+	return &ProbeSkbuff{
+		len: size,
+	}
 }
 
+// ProbeSkbuffPeek returns nil for an empty list or a pointer to the head element
 func ProbeSkbuffPeek(head *probeSkbuffHead) *ProbeSkbuff {
-
+	skb := head.next
+	if skb == nil {
+		return nil
+	}
+	return skb
 }
 
-func ProbeSkBuffDeinit(skb *ProbeSkbuff) {
-
+func ProbeSkbuffDeinit(skb *ProbeSkbuff) {
+	skb = nil
 }
 
-func ProbeSkbuffPush(skb *ProbeSkbuff, len uint) uint8 {
-
+func ProbeSkbuffPush(skb *ProbeSkbuff, len uint) uint {
+	skb.data -= len
+	skb.len += len
+	return skb.data
 }
 
 func ProbeSkbuffReserve(skb *ProbeSkbuff, len uint) {
-
+	skb.data += len
+	skb.tail += len
 }
 
 func ProbeSkbuffResetHead(skb *ProbeSkbuff) {
-
+	skb.head = 0
 }
 
-func ProbeSkbuffGetHead(skb *ProbeSkbuff) uint8 {
+func ProbeSkbuffGetHead(skb *ProbeSkbuff) uint {
+	return skb.head
+}
 
+// ProbeSkbuffUnlink is used to remove a SKB from the list
+func ProbeSkbuffUnlink(skb *ProbeSkbuff, list *probeSkbuffHead) {
+	var next, prev *ProbeSkbuff
+
+	list.qlen--
+	next = skb.next
+	prev = skb.prev
+	skb.next = nil
+	skb.prev = nil
+	next.prev = prev
+	prev.next = next
 }
 
 /*
  * Probe sk buff datagram/head queue function design
-*/
+ */
+
+// ProbeSkbuffInsert is used to insert ProbeSkbuff into a list
+func ProbeSkbuffInsert(newsk *ProbeSkbuff, prev *ProbeSkbuff, next *ProbeSkbuff, list *probeSkbuffHead) {
+	newsk.next = next
+	newsk.prev = prev
+	next.prev = newsk
+	prev.next = newsk
+	list.qlen++
+}
 
 func ProbeSkbuffQueueInit(head *probeSkbuffHead) {
-
+	head.next = nil
+	head.prev = nil
+	head.qlen = 0
 }
 
 func ProbSkbuffQueueDeinit(head *probeSkbuffHead) {
+	head.next = nil
+	head.prev = nil
+	head.qlen = 0
 }
 
+// ProbeSkbuffDequeue is used to remove the first SKB from the queue
 func ProbeSkbuffDequeue(head *probeSkbuffHead) *ProbeSkbuff {
-
+	skb := ProbeSkbuffPeek(head)
+	if skb != nil {
+		ProbeSkbuffUnlink(skb, head)
+	}
+	return skb
 }
 
+// ProbeSkbuffQueueAdd is used to add new SKB before next SKB
 func ProbeSkbuffQueueAdd(head *probeSkbuffHead, new *ProbeSkbuff, next *ProbeSkbuff) {
-
+	ProbeSkbuffInsert(new, next.prev, next, head)
 }
 
+// ProbeSkbuffQueueAddEnd is used to add
 func ProbeSkbuffQueueAddEnd(head *probeSkbuffHead, new *ProbeSkbuff) {
-
+	ProbeSkbuffQueueAdd(head, new, head.prev)
 }
 
+// ProbeSkbuffQueueCheckEmpty return 1 if the queue is empty, 0 otherwise
 func ProbeSkbuffQueueCheckEmpty(head *probeSkbuffHead) int {
-
+	if head.next == nil {
+		return 1
+	} else {
+		return 0
+	}
 }
 
-func ProbeSkbuffQueueGetLen(head *probeSkbuffHead) uint32 {
-
+// ProbeSkbuffQueueGetLen returns the length of the sk_buff queue
+func ProbeSkbuffQueueGetLen(head *probeSkbuffHead) uint {
+	return head.qlen
 }
